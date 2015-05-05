a59c06bcdf6ee3ee0288d7291160f6c73124e571
Nick Jensen <nickrjensen@gmail.com>
2015-05-05 12:54:04 -0400

    removing some test code

--- Cumulus/AppDelegate.m
+++ Cumulus/AppDelegate.m
@@ -57,11 +57,7 @@ NSString * const CUErrorDomain = @"io.nrj.cumulus.error";
         [NSApp activateIgnoringOtherApps:YES];
         [[self window] makeKeyAndOrderFront:nil];
     }
-#endif
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [[self window] makeFirstResponder:nil];
-    });
+#endif    
 }
 
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)theApplication hasVisibleWindows:(BOOL)flag {
